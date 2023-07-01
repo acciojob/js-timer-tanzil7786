a78e06cfae9ab2a388162592e592a6dff29948d8
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-07-01 23:11:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-const players = ["John", "Bob", "Alice", "Poppy"];
+function formatDateTime(date) {
+  let day = String(date.getDate()).padStart(2, '0');
+  let month = String(date.getMonth() + 1).padStart(2, '0');
+  let year = date.getFullYear();
+  let hours = String(date.getHours()).padStart(2, '0');
+  let minutes = String(date.getMinutes()).padStart(2, '0');
+  let seconds = String(date.getSeconds()).padStart(2, '0');
+  return `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+}
 
-const person = {
-  name: "John Doe",
-  age: 80,
-};
+// Function to update the timer display
+function updateTimer() {
+  let timerElement = document.getElementById('timer');
+  let currentDate = new Date();
+     timerElement.innerText = formatDateTime(currentDate);
+}
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Update the timer initially
+updateTimer();
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+// Update the timer every second
+setInterval(updateTimer, 1000);
